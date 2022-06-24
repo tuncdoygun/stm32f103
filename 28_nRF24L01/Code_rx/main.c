556a6ef70d2af7831b601d04ae469eec59baaff6
tuncdoygun <64736382+tuncdoygun@users.noreply.github.com>
2022-06-25 00:31:05 +0300

    nrf24l01 rx init

---
+++ 28_nRF24L01/Code_rx/main.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+
+#include "system.h"
+#include "io.h"
+#include "uart.h"
+#include "spi.h"
+#include "nRF24.h"
+
+void init(void)
+{
+  // System Clock init
+  Sys_ClockInit();
+  
+  // I/O portlarý baþlangýç
+  Sys_IoInit();
+  
+  // LED baþlangýç
+  IO_Write(IOP_LED, 1); // 0 olarak baþlatmak için
+  IO_Init(IOP_LED, IO_MODE_OUTPUT);
+  
+  // Console baþlangýç
+  Sys_ConsoleInit();  
+  
+  SPI_Start();
+    
+  NRF24_begin();
+}
+
+// 29.07.2021
+// yukardakine göre daha optimize
+void Task_LED(void)
+{
+  static enum {
+    I_LED_OFF,// led_off durumuna geçerken ilk yapýlacaklar.baþlangýçta yapýlacak iþler çok fazlaysa, nefes aldýrmak için daha faydalý.
+    S_LED_OFF,
+    I_LED_ON,
+    S_LED_ON,
+  } state = I_LED_OFF;
+   
+  static clock_t t0, t1; // t0 duruma ilk geçiþ saati, t1 güncel saat
+  
+  t1 = clock(); // bu fonksiyona girdiðinde o andaki saat
+  
+  switch (state){
+  case I_LED_OFF:
+    t0 = t1;
+    IO_Write(IOP_LED, 0);
+    state = S_LED_OFF;
+    //break;
+  case S_LED_OFF:
+    if (t1 >= t0 + 9 * CLOCKS_PER_SEC / 10){ // 9/10 saniye geçmiþ demek
+      state = I_LED_ON;
+    }
+    break;
+    
+  case I_LED_ON:
+    t0 = t1;
+    IO_Write(IOP_LED, 1);
+    state = S_LED_ON;
+    //break;
+  case S_LED_ON:
+    if (t1 >= t0 +  CLOCKS_PER_SEC / 10){ // 9/10 saniye geçmiþ demek
+      state = I_LED_OFF;
+    }    
+    break;
+  }
+}
+//
+
+void Task_Print(void)
+{
+  static unsigned count;
+  
+  printf("SAYI:%10u\r", ++count);
+}
+
+int main()
+{
+  uint64_t RxpipeAddrs = 0x11223344AA;
+  char myRxData[50];
+  char myAckPayload[32] = "Ack by STMF1!";  
+  
+  init();
+   
+  NRF24_setAutoAck(true);
+  NRF24_setChannel(52);
+  NRF24_setPayloadSize(32);
+  NRF24_openReadingPipe(1, RxpipeAddrs);
+  NRF24_enableDynamicPayloads();
+  NRF24_enableAckPayload();
+	
+  NRF24_startListening(); 
+  
+  printRadioSettings();
+  
+  while (1)
+  {
+    Task_LED();  
+    
+    if(NRF24_available())
+    {
+      NRF24_read(myRxData, 32);
+      NRF24_writeAckPayload(1, myAckPayload, 32);
+      printf("%s \r\n", myRxData);
+    }
+  }
+}
+
+
